61365ff3f72c5e5f40f02a0e976c500204d6130f
muhammed65yasar <107045897+muhammed65yasar@users.noreply.github.com>
2022-06-07 15:15:32 +0300

    Add files via upload

---
+++ sınav  muhammed/script.js
@@ -0,0 +1,15 @@
+let sayı = [Input.queryselector(txtsayı).value]
+let sayı = [Input.queryselector(oynabtn).value]
+let sayı = [Input.queryselector(sıfırlabtn).value]
+Math.floor(math.random()*100)
+
+function sayıyakala(){
+    document.addEventListener("click", oynabtn)
+   
+    
+}
+
+function sıfırla(){ 
+    document.getElementById("sıfırlabtn").value
+    document.addEventListener("click","sıfırlabtn").value
+}
